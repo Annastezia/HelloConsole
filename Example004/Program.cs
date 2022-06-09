c3bfad9b9a4f752433309a4d490b63b6e232f337
Anastasia <nastasy.kozh@gmail.com>
2022-06-09 03:31:16 +0300

    Example 004

--- Example004/Program.cs
+++ Example004/Program.cs
@@ -5,3 +5,9 @@ Console.WriteLine(numberA / numberB);
 double numberC = 12;
 double numberD = 5;
 Console.WriteLine(numberC / numberD);
+
+int numberE = new Random().Next(0,10);
+Console.WriteLine(numberE);
+int numberF = new Random().Next(0,10);
+Console.WriteLine(numberF);
+Console.WriteLine(numberE+numberF);
